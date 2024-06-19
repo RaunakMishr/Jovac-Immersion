8d582b7a5632ef5ed325384963be30ec6d2eb452
RaunakMishr <122252610+RaunakMishr@users.noreply.github.com>
2024-06-19 23:07:38 +0500

    Add files via upload

---
+++ Day5/climbingStairs.java
@@ -0,0 +1,22 @@
+package Day5;
+
+public class climbingStairs {
+    public static void main(String[] args) {
+        int n = 4;
+        print(n,"");
+    }
+    private static void print(int n, String ans){
+        if(n == 0){
+            System.out.println(ans);
+            return;
+        }
+        if(n<0){
+            return;
+        }
+
+        print(n-1, ans+1);
+        print(n-2, ans+2);
+        print(n-3, ans+3);
+        
+    }
+}
